70d358a31bed7230b8c8e083c3496819e7badc64
kartiksathappan <kartiksathappan@gmail.com>
2024-03-08 12:17:51 -0800

    font is working!

--- api/index.tsx
+++ api/index.tsx
@@ -1,12 +1,31 @@
 import { Button, Frog } from 'frog';
 import { handle } from 'frog/vercel';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config({ path: '.env.local' });
+
+const fontUrl = './public/arial-narrow.ttf';
+
+
+// export const app = new Frog({
+//   assetsPath: '/',
+//   basePath: '/api',
+// });
 
 export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
+  imageOptions: {
+    fonts: [
+      {
+        name: 'Arial Narrow',
+        data: fs.readFileSync(fontUrl),
+        weight: 400,
+        style: 'normal',
+      },
+    ],
+  },
 });
 
 async function findAddressByFid(fid: string | undefined) {
